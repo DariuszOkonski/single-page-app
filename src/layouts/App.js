d4fd5586539ef504f7183b4d1acd0d5962ebf271
Dariusz Okonski <darek200180@gmail.com>
2019-09-20 07:06:45 +0100

    add Header, Navigation, Page and Footer components

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import '../styles/App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './Header';
+import Navigation from './Navigation';
+import Page from './Page';
+import Footer from './Footer';
 
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
-        <h1>Single Page App</h1>
+        <header>
+          {<Header />}
+        </header>
+
+        <main>
+          <aside>
+            {<Navigation />}
+          </aside>
+          <section className="page">
+            {<Page />}
+          </section>
+        </main>
+
+        <footer>
+          {<Footer />}
+        </footer>
       </div>
     </Router>
   );
