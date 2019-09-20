c914f0f113514775ebedfd1ee83370795e39b195
Dariusz Okonski <darek200180@gmail.com>
2019-09-20 06:46:06 +0100

    Updates

---
+++ precache-manifest.8c622ffbeb4c0d40fc6e7f63e2ddf6eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8edbde1a6659eba10bbd3c7307622995",
+    "url": "/single-page-app/index.html"
+  },
+  {
+    "revision": "1b2617cae7d1c0027a24",
+    "url": "/single-page-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "664cbc248409235d6b49",
+    "url": "/single-page-app/static/js/2.7fdb6de3.chunk.js"
+  },
+  {
+    "revision": "1b2617cae7d1c0027a24",
+    "url": "/single-page-app/static/js/main.c7016fe1.chunk.js"
+  },
+  {
+    "revision": "bbac4ab0efbc878aaf90",
+    "url": "/single-page-app/static/js/runtime-main.cb073895.js"
+  }
+]);
