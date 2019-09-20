f9ef072e9b1b47d432e89f83938441090807ae2d
Dariusz Okonski <darek200180@gmail.com>
2019-09-20 14:06:08 +0100

    implement logic at ContactPage component and add styles at Page.css for it

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import { Prompt } from 'react-router-dom';
+import '../styles/Page.css';
 
-const ContactPage = () => {
-  return (
-    <h1>ContactPage</h1>
-  );
+class ContactPage extends React.Component {
+  state = {
+    value: "",
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      value: '',
+    })
+  }
+
+  render() {
+    return (
+      <div className="contact">
+        <form onSubmit={this.handleSubmit}>
+          <h3>Napisz do nas</h3>
+
+          <textarea
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="Type something..."
+          ></textarea>
+          <button>Send</button>
+        </form>
+
+        <Prompt
+          when={this.state.value.length > 0}
+          message='Are you sure you want to leave?'
+        />
+
+      </div>
+    );
+  }
+
 }
 
 export default ContactPage;
